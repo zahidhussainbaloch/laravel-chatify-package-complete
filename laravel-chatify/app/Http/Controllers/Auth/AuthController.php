a6b7d1ced5fadf9158925b77f93d4301ac359b0a
Zahid Hussain <zahidhussainzh065@gmail.com>
2023-11-28 19:29:07 +0500

    Laravel chat apllication using laravel package Chatify

---
+++ laravel-chatify/app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+
+use Session;
+
+use App\Models\User;
+
+use Hash;
+
+
+class AuthController extends Controller
+{
+    public function index()
+
+    {
+
+        return view('auth.login');
+
+    }
+
+    public function registration()
+
+    {
+
+        return view('auth.registration');
+
+    }
+
+    public function postLogin(Request $request)
+
+    {
+
+        $request->validate([
+
+            'email' => 'required',
+
+            'password' => 'required',
+
+        ]);
+
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+
+            return redirect()->intended('dashboard')
+
+                        ->withSuccess('You have Successfully loggedin');
+
+        }
+
+
+
+        return redirect("login")->withSuccess('Oppes! You have entered invalid credentials');
+
+    }
+
+    public function postRegistration(Request $request)
+
+    {
+
+        $request->validate([
+
+            'name' => 'required',
+
+            'email' => 'required|email|unique:users',
+
+            'password' => 'required|min:6',
+
+        ]);
+
+
+
+        $data = $request->all();
+
+        $check = $this->create($data);
+
+
+
+        return redirect("dashboard")->withSuccess('Great! You have Successfully loggedin');
+
+    }
+
+    public function dashboard()
+
+    {
+
+        if(Auth::check()){
+
+            // return view('dashboard');
+            return Redirect('chatify');
+
+        }
+
+
+
+        return redirect("login")->withSuccess('Opps! You do not have access');
+
+    }
+
+    public function create(array $data)
+
+    {
+
+      return User::create([
+
+        'name' => $data['name'],
+
+        'email' => $data['email'],
+
+        'password' => Hash::make($data['password'])
+
+      ]);
+
+    }
+
+    public function logout() {
+
+        Session::flush();
+
+        Auth::logout();
+
+
+
+        return Redirect('login');
+
+    }
+
+
+
+}
